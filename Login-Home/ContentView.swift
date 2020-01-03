851185c041745cc5f5fb2cf4e8ead78ab3b86f71
Mufakkharul Islam Nayem <mufakkharul.nayem@bjitgroup.com>
2020-01-03 09:05:16 +0600

    Initial Commit

---
+++ Login-Home/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Login-Home
+//
+//  Created by Mufakkharul Islam Nayem on 1/3/20.
+//  Copyright © 2020 Mufakkharul Islam Nayem. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
